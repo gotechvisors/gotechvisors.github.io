03d7c0174fb65b797dffc9b26e8b0524c259912a
adamjkwong <adamjkwong@gmail.com>
2020-02-21 09:21:03 -0800

    changes to main page

--- goTechVisors.js
+++ goTechVisors.js
@@ -1,6 +1,4 @@
-<script>
-  function payNow() { 
-      window.open( 
-        "https://www.paypal.com/paypalme2/apangolin?utm_source=unp&utm_medium=email&utm_campaign=PPC000628&utm_unptid=800bcd5b-52b4-11ea-b05a-b875c0f50354&ppid=PPC000628&cnac=US&rsta=en_US&cust=EYNBG96PBRJHE&unptid=800bcd5b-52b4-11ea-b05a-b875c0f50354&calc=5e9d8c388a510&unp_tpcid=ppme-social-user-profile-created&page=main:email:PPC000628:::&pgrp=main:email&e=cl&mchn=em&s=ci&mail=sys", "_blank"); 
-  } 
-</script>
+function payNow() { 
+  window.open( 
+    "https://www.paypal.com/paypalme2/apangolin/50", "_blank"); 
+} 
