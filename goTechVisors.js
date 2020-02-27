02dd22057b4dc73899bbed18e567e33d8802ab2e
adamjkwong <adamjkwong@gmail.com>
2020-02-26 19:57:35 -0800

    fixed internal page linking

--- goTechVisors.js
+++ goTechVisors.js
@@ -2,3 +2,18 @@ function payNow() {
   window.open( 
     "https://www.paypal.com/paypalme2/apangolin/50", "_blank"); 
 } 
+
+function scheduleFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+  var text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
